1a8086a3eb7a2bb03601610011c03b8de059e1d8
Jesus <jesusanesredondo@gmail.com>
2025-07-31 10:36:40 +0200

    Restaurante-Render

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ composer install
 
 # Ejecutar las migraciones
 php bin/console doctrine:database:create --if-not-exists
+
+php bin/console doctrine:migrations:diff
+
 php bin/console doctrine:migrations:migrate --no-interaction
 
 # Ejecutar el comando final del contenedor (por ejemplo: php -S ...)
